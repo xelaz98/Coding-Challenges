def1ede7c87913d5f87a69d9c3554ad0f5180ff1
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-01-17 15:44:58 +0200

    add 28th coding challenge

---
+++ ex28.js
@@ -0,0 +1,12 @@
+// 28. Write a JavaScript program to pass a 'JavaScript function' as a parameter.
+
+function addStudent(id, refreshCallback)
+{
+    refreshCallback();  
+}
+
+function refreshStudentList() {
+    console.log('Hello');
+}
+
+addStudent(1, refreshStudentList);
