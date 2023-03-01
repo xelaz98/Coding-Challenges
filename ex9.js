322b8e4474a1d20053d7a88396c3faf3cdf75038
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-03-01 15:03:59 +0200

    add 9th coding challenge

---
+++ ex9.js
@@ -0,0 +1,27 @@
+// 8. Write a JavaScript function that accepts a number as a parameter and checks whether it is prime or not.
+// Note : A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+function test_prime(n)
+{
+
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+
+console.log(test_prime(37));
