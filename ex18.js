e051e0b31f7cc59f5b44b3a4002017b7f189e1eb
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-03-17 15:14:00 +0200

    add 18th coding challenge

---
+++ ex18.js
@@ -0,0 +1,9 @@
+// 17. Write a JavaScript function to get the number of occurrences of each letter in a specified string.
+
+function Char_Counts(str1) {
+    var uchars = {};
+    str1.replace(/\S/g, function(l){uchars[l] = (isNaN(uchars[l]) ? 1 : uchars[l] + 1);});
+    return uchars;
+    }
+    console.log(Char_Counts("The quick brown fox jumps over the lazy dog"));
+    
