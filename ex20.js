b667f5bbac369f6308516983b4803f2aa887cc8d
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-03-20 15:14:30 +0200

    add 20th coding challenge

---
+++ ex20.js
@@ -0,0 +1,11 @@
+// 19. Write a JavaScript function that returns array elements larger than a number.
+
+function BiggerElements(val)
+   {
+     return function(evalue, index, array)
+     {
+     return (evalue >= val);
+     };
+   }
+var result = [11, 45, 4, 31, 64, 10]. filter(BiggerElements(10));
+console.log(result);
