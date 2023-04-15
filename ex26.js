0201858ec00ef8b3c31016abb4fcd441ec163930
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-04-15 16:28:08 +0300

    add  26th coding challenge

---
+++ ex26.js
@@ -0,0 +1,13 @@
+// 25. Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
+// Sample function : Longest_Country_Name(["Australia", "Germany", "United States of America"])
+// Expected output : "United States of America"
+
+function Longest_Country_Name(country_name)
+  {
+  return country_name.reduce(function(lname, country) 
+  {
+    return lname.length > country.length ? lname : country;
+  }, 
+"");
+}
+console.log(Longest_Country_Name(["Australia", "Germany", "United States of America"]));
