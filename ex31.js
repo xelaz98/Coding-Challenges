17bf176dbe5c8cf1c8c967d9929dd9a7125acd28
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-06-29 17:28:47 +0300

    add coding challenge

---
+++ ex31.js
@@ -0,0 +1,9 @@
+// 154. Write a JavaScript program to create an object composed of the properties the given function returns false for. The function is invoked with two arguments: (value and key).
+
+//#Source https://bit.ly/2neWfJ2 
+const omitBy = (obj, fn) =>
+  Object.keys(obj)
+    .filter(k => !fn(obj[k], k))
+    .reduce((acc, key) => ((acc[key] = obj[key]), acc), {});
+
+console.log(omitBy({ a: 1, b: '2', c: 3 }, x => typeof x === 'number'));
